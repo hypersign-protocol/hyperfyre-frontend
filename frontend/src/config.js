ce795aa0c13f5f210108b87e3209aa453b05caf2
vishwas anand <vishwasanand.cse@gmail.com>
2021-05-19 15:33:45 +0530

    updated config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,7 +5,7 @@ const config = {
     app: {
         name: process.env.VUE_APP_TITLE || "Dalmia Tracebil",
         decription: process.env.VUE_APP_DESC,
-        version: process.env.VERSION
+        version: process.env.VUE_APP_VERSION
     },
 
 }
