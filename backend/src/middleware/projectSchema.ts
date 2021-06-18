86859b35128eb491100d954787fa9292605563c5
vishwas anand <vishwasanand.cse@gmail.com>
2021-06-18 18:12:04 +0530

    upddated error message

--- backend/src/middleware/projectSchema.ts
+++ backend/src/middleware/projectSchema.ts
@@ -12,38 +12,38 @@ export const ProjectSchemaBody = [
   body("projectName")
     .exists({ checkFalsy: true })
     .trim()
-    .withMessage("projectName can not be null or empty"),
+    .withMessage("projectName is null or empty"),
   body("logoUrl")
     .exists({ checkFalsy: true })
     .trim()
-    .withMessage("logoUrl can not be null or empty"),
+    .withMessage("logoUrl is invalid or empty"),
 
   body("fromDate")
     .exists({ checkFalsy: true })
     .trim()
     .custom((value) => validateDate(value))
-    .withMessage("fromDate can not be null or empty"),
+    .withMessage("fromDate is invalid or empty"),
   body("toDate")
     .exists({ checkFalsy: true })
     .trim()
     .custom((value) => validateDate(value))
-    .withMessage("toDate can not be null or empty"),
+    .withMessage("toDate is invlaid or empty"),
 
   body("twitterHandle")
     .exists({ checkFalsy: true })
     .trim()
-    .withMessage("twitterHandle can not be null or empty"),
+    .withMessage("twitterHandle is null or empty"),
   body("telegramHandle")
     .exists({ checkFalsy: true })
     .trim()
-    .withMessage("telegramHandle can not be null or empty"),
+    .withMessage("telegramHandle is null or empty"),
   body("twitterPostFormat")
     .exists({ checkFalsy: true })
     .trim()
-    .withMessage("twitterPostFormat can not be null or empty"),
+    .withMessage("twitterPostFormat is null or empty"),
 
   body("projectStatus")
     .exists({ checkFalsy: true })
     .trim()
-    .withMessage("projectStatus can not be null or empty"),
+    .withMessage("projectStatus must be selected"),
 ];
