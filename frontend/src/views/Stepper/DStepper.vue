5ad3ec6f927a2e906f2475356765356cc6dc8ebb
vishwas anand <vishwasanand.cse@gmail.com>
2021-07-08 23:02:55 +0530

    added logout

--- frontend/src/views/Stepper/DStepper.vue
+++ frontend/src/views/Stepper/DStepper.vue
@@ -9,8 +9,11 @@
     ></loading>
 
     <div class="header w-100 text-left" :style="`background-color: ${projectDetails.themeColor}; color: ${projectDetails.fontColor}`">
-      <div class="logo  d-inline-block">
-        <img  :src="projectDetails.logoUrl"  />
+      <div class="logo  d-inline-block" style="width:100%">
+        <img :src="projectDetails.logoUrl" style="float:left"/>
+        <button @click="logout" class="btn text-dark" style="float:right">
+          Logout
+        </button>
       </div>
       <div class="text mx-auto  py-3 text-left" v-if="step != 3">
         <h4 class="mb-4">
@@ -315,6 +318,14 @@ export default {
   },
 
   methods: {
+
+    logout() {
+      localStorage.removeItem("authToken");
+      localStorage.removeItem("user");
+      localStorage.removeItem("credentials");
+      localStorage.removeItem("userData");
+      this.$router.push("/login");
+    },
 
   checkBlockChainType(){
 
@@ -672,6 +683,14 @@ export default {
 </script>
 
 <style>
+.btn{
+  border: 1px solid #494949;
+  margin: 20px 0;
+  bottom: 0;
+  right: 0;
+}
+
+
 .d-stepper .d-stepper-header {
   max-width: 600px;
   margin: 0 auto;
