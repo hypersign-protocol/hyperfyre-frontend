bd24baf8585d53545b95c61e9e118128d1c633ca
vishwas anand <vishwasanand.cse@gmail.com>
2021-07-20 02:16:10 +0530

    error handled

--- backend/src/services/subscription.service.ts
+++ backend/src/services/subscription.service.ts
@@ -8,17 +8,25 @@ export default class Subscription{
     
     add({planId, userDid, subscriptionDate, isActive, hasExpired, leftOverNoRequests}): Promise<ISubscription>{
         return new Promise(async (resolve, reject) => {
-            const newPlan: ISubscription = await SubscriptionModel.create({
-                planId, userDid, subscriptionDate, isActive, hasExpired, leftOverNoRequests          
-            });
-            resolve(newPlan);
+            try{
+                const newPlan: ISubscription = await SubscriptionModel.create({
+                    planId, userDid, subscriptionDate, isActive, hasExpired, leftOverNoRequests          
+                });
+                resolve(newPlan);
+            }catch(e){
+                reject(e)
+            }
         })
     }
 
     list({did}): Promise<Array<ISubscription>>{
-        return new Promise(async (resolve) => {
-            const allSubscriptionForUser: Array<ISubscription> = await SubscriptionModel.where({did: did}).find();
-            resolve(allSubscriptionForUser);
+        return new Promise(async (resolve, reject) => {
+            try{
+                const allSubscriptionForUser: Array<ISubscription> = await SubscriptionModel.where({did: did}).find();
+                resolve(allSubscriptionForUser);
+            }catch(e){
+                reject(e)
+            }
         })
     }
 }
