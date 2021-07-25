f3b5ca1dbc5f15d46fabd45789f0070d97d41e44
vishwas anand <vishwasanand.cse@gmail.com>
2021-07-25 12:35:19 +0530

    added some design to webstie

--- frontend/src/views/Website.vue
+++ frontend/src/views/Website.vue
@@ -1,14 +1,33 @@
 <template>
-    <div>
-        <h1>The site is under construction!</h1>
-        <h4>Kindly come back after sometime...</h4>
-
-    <p>
-        <ul>
-            <li><a href="/app" target="_blank">Admin Dashboard</a></li>
-            <li><a href="#" target="_blank">Docs</a></li>
-        </ul>
-    </p>
+    <div class="body">
+        <div class="row">
+            <div class="col-md-4">
+                <img
+                    :src="require('./assets/Fidato_logo.png')"
+                    alt="logo"
+                    width="150px"
+                />
+            </div>
+            <div class="col-md-8">
+                <button class="btn btn-default btn-lg">
+                    <a href="/app" target="_blank">Try Now</a>
+                </button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12" style="widht:100%; height:100%">
+                <h1>FIDATO</h1>
+                <h4>Build Resilient and Dependable Communities Securely</h4>
+            </div>
+        </div>
         
     </div>
 </template>
+<style lang="scss" scoped>
+
+    .body{
+        background: black;
+        color: whitesmoke;
+    }
+
+</style>
