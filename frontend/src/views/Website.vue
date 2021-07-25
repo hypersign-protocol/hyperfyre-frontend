7c2e1e13fd4ae9dc2b782625371c9ad71847855c
vishwas anand <vishwasanand.cse@gmail.com>
2021-07-25 11:26:08 +0530

    adde nav in website

--- frontend/src/views/Website.vue
+++ frontend/src/views/Website.vue
@@ -2,5 +2,13 @@
     <div>
         <h1>The site is under construction!</h1>
         <h4>Kindly come back after sometime...</h4>
+
+    <p>
+        <ul>
+            <li><a href="/app" target="_blank">Admin Dashboard</a></li>
+            <li><a href="#" target="_blank">Docs</a></li>
+        <ul>
+    </p>
+        
     </div>
 </template>
