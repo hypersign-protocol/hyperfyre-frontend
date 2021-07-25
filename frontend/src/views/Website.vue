cb0757f972c32ee95ca95b180e905cd22d22a479
vishwas anand <vishwasanand.cse@gmail.com>
2021-07-25 13:24:21 +0530

    added very basic design

--- frontend/src/views/Website.vue
+++ frontend/src/views/Website.vue
@@ -8,31 +8,31 @@
                     width="150px"
                 />
             </div>
-            <div class="col-md-8">
+            <!-- <div class="col-md-8">
                 <button class="btn btn-default btn-lg">
                     <a href="/app" target="_blank">Try Now</a>
                 </button>
-            </div>
+            </div> -->
         </div>
         <div class="row content">
-            <div class="col-md-12" style="widht:100%; height:100%">
-                <h1>FIDATO</h1>
+            <div class="col-md-12" style="">
+                <h1 class="bold">FIDATO</h1>
                 <h4>Build Resilient and Dependable Communities Securely</h4>
                 <button class="btn btn-default btn-lg">
-                    <a href="/app" target="_blank">Try App Now</a>
+                    <a href="/app" target="_blank">TRY APP NOW</a>
                 </button>
             </div>
         </div>
-        <div class="row footer">
+        <!-- <div class="row footer">
             <div class="col-md-4">
-                <p>Powered By</p>
+                <span>Powered By</span>
                 <img
                     :src="require(`../assets/footerLogo.png`)"
-                    style="max-width: 80px;"
+                    style="max-width: 100px;"
                 />
             </div>
             <div class="col-md-8"></div>
-        </div>
+        </div> -->
     </div>
 </template>
 <style scoped>
@@ -40,6 +40,34 @@
     .body{
         background: black;
         color: whitesmoke;
+        height: 100%;
+        top: 0px;
+        right: 0px;
+        bottom: 0px;
+        left: 0px;
+        position: absolute;
+    }
+
+    .content{
+        font-variant: all-petite-caps;
+        text-align: center;
+        margin-top: 18%;
+    }
+
+    .header{
+        top: 10px;
+        left: 10px;
+        position: absolute;
+    }
+
+    .footer{
+        bottom: 10px;
+        left: 10px;
+        position: absolute;
+    }
+    .content .bold{
+        font-weight: bolder;        
+        font-size: 700%;
     }
 
 </style>
