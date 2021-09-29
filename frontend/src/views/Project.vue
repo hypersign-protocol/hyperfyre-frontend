a1b560ba7c780defa23eb42b37f245b95e89c6c4
Pratap Mridha <pratapmridha@gmail.com>
2021-09-29 20:20:35 +0530

    Fixed Search Issue (case insensitive)

--- frontend/src/views/Project.vue
+++ frontend/src/views/Project.vue
@@ -509,10 +509,9 @@ export default {
       console.log("Event receieved from grand-child")
     },
     handleSearch(e){
-
         if(e.target.value.length){
           this.searchQuery = e.target.value.trim();
-          return this.projectsToShow = this.projects.filter(x => x.projectName.includes(e.target.value));
+          return this.projectsToShow = this.projects.filter(x => (x.projectName.toLowerCase().includes(e.target.value.toLowerCase())));
         } else{
           this.searchQuery = ""
           this.paginateChange(this.currentPage)
