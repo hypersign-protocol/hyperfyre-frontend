edf90fd9b9d0f2ee921c8cc0240d84f398b1f76c
rajpatil6939 <raj.v.patil108@gmail.com>
2021-10-04 11:55:06 +0530

    All Message added for Telegram in en file in TELEGRAM_JOIN at participant side

--- frontend/src/utils/messages/participants/en.js
+++ frontend/src/utils/messages/participants/en.js
@@ -21,7 +21,10 @@ const EVENT_ACTIONS = {
         INVALID_DATE_TIME: "Error: Invalid date time"
     },
     TELEGRAM_JOIN: {
-
+        TELEGRAM_AUTH: "Error: Please authorize telegram to proceed",
+        AUTH_FAILED: "Authentication Failed! Try again",
+        FETCH_USERNAME_FAILED: "Could not fetch the username after telegram authentication",
+        ERROR: "Error occurred: "
     },
     TWITTER_FOLLOW: {
 
