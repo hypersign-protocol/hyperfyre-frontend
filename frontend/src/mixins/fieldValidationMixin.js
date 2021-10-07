034e92a0fdddd64ad06497e103c4601791785892
rajpatil6939 <raj.v.patil108@gmail.com>
2021-10-07 17:34:49 +0530

    urlSanitizer added for frontend(issues/419 fixed)

--- frontend/src/mixins/fieldValidationMixin.js
+++ frontend/src/mixins/fieldValidationMixin.js
@@ -62,3 +62,23 @@ export function truncate(str, limit){
   const lastPart = str.slice(-eachLen)
   return firstPart + " ... " + lastPart;
 }
+
+
+export function urlSanitizer(url,endsWith){
+
+  switch (endsWith) {
+    case true:
+      if(url.endsWith('/'))
+        return url
+      else
+        return url+'/'
+      break;
+    case false:
+        if(url.endsWith('/'))      
+          return url.slice(0,-1)
+      else
+        return url
+      break;
+  }
+  
+}
