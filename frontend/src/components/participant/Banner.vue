dbd5332b119816fbf1bb6ac0a3625e84bea71cb3
vishwas anand <vishwasanand.cse@gmail.com>
2021-10-08 13:30:06 +0530

    updated banaer default text

--- frontend/src/components/participant/Banner.vue
+++ frontend/src/components/participant/Banner.vue
@@ -1,7 +1,7 @@
 <template>
   <b-row>
     <b-col md="12">
-      <b-card no-body class="banner rounded-0" :img-src="logoUrl" img-alt="Card Image">
+      <b-card no-body class="banner rounded-0" :img-src="logoUrl" img-alt="Could not fetch banner image :(">
       </b-card>
     </b-col>
   </b-row>
