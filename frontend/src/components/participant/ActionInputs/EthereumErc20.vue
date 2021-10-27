00e3fa2030574b00b02b98527ba44067b38931c9
Pratap Mridha <pratapmridha@gmail.com>
2021-10-27 23:38:23 +0530

    fixed some bug

--- frontend/src/components/participant/ActionInputs/EthereumErc20.vue
+++ frontend/src/components/participant/ActionInputs/EthereumErc20.vue
@@ -14,41 +14,43 @@
         <b-col cols="9" sm="9" class="text-left" md="9">
           <div class="text text-capitalize">{{ data.title }}</div>
         </b-col>
+       
         <b-col cols="2" sm="2" md="2">
-          <b-badge class="btn-score" @click="update()" v-if="done">
+          <b-badge class="btn-score" @click="update()" v-if="!done">
             <img src="../../../assets/plus.svg" />
             {{ data.score }}
           </b-badge>
-           <!-- <b-badge class="btn-score" @click="invokeMetamask()" v-if="!done">
-            <img src="../../../assets/metamask.svg" />
-            {{ data.score }}
-          </b-badge> -->
-          <!-- <img
+          <img
             class="check-mark"
             src="../../../assets/check-circle-fill.svg"
             height="25px"
-            {{data.score}}
             v-if="done"
-          /> -->
+          />
         </b-col>
         
       </b-row>
     </b-card-header>
     <b-collapse :id="`collapse-${idValue}`" v-model="visible">
       <b-card-body class="user-details">
-        <b-row>
+        <b-row v-if="!showerror">
           <b-col cols="12" sm="12" md="12">
             <div class="metamask">
               <b-form-input
                 type="text"
                 :placeholder="data.placeHolder"
                 v-model="data.value"
-                :disabled="done"
+                :disabled="true"
                 :required="data.isManadatory"
               ></b-form-input>
              <button
              class="btn btn-outline-twitter text-black" @click="invokeMetamask()"> <img src="../../../assets/metamask.svg" height="25px" width="25px" /></button>
           </div>
+            
+          </b-col>
+        </b-row>
+         <b-row v-else>
+          <b-col cols="12" sm="12" md="12">
+            Please Install metamask
             
           </b-col>
         </b-row>
@@ -68,7 +70,6 @@ import {
 } from "../../../mixins/fieldValidationMixin";
 import notificationMixins from "../../../mixins/notificationMixins";
 import Messages from "../../../utils/messages/participants/en";
-// import validateEthAddress from "../../../mixins/fieldValidationMixin"
 import Web3 from 'web3';
 export default {
   name: "EthereumErc20",
@@ -85,52 +86,56 @@ export default {
       visible: false,
       done: this.data.isDone,
       authToken: localStorage.getItem("authToken"),
-
+      showerror:false,
+      signature:"",
+      message_sign:"",
+      
     };
   },
-  mounted() {
-    // this.metamask();
-    // this.web33();
+  mounted() {    
     eventBus.$on(`disableInput${this.data._id}`, this.disableInput);
     this.data.contractAddress=this.data.value
     this.data.value=""
-    
-    
-
+    this.checkWeb3Injection()
   },
   methods: {
-    // ans(){this.$root.$on("in",()=>{
-    //    console.log("");
-    //  })},
-// metamask(){
-//   if (!window.ethereum) {
-//   return this.notifyErr("Please install MetaMask");
-//   }else{
-// console.log("installed");
-//   }
-  
-// },
-// web33(){
-//   var web3 = new Web3(Web3.givenProvider);
-// console.log(web3);
-
-// },
-invokeMetamask(){
- if (!window.ethereum) {
-  return this.notifyErr("Please install MetaMask");
-  }else{
-    ethereum.enable();
-  }
+    checkWeb3Injection(){
+      try {
+        if(ethereum && ethereum.isMetaMask){
+          this.web3=new Web3(window.ethereum)    
+          console.log(this.web3);
+        } 
+        } catch (error) {
+          console.log(error);
+           this.showerror=true
+        }
+    },
+    async signMessage(){
+      const message="You are Signing this Message and Ensuring you participation in this project";
+      this.message_sign=message
+      return await this.web3.eth.personal.sign(message,ethereum.selectedAddress)
+    },
+  async invokeMetamask(){
+      try {
+        if (ethereum.isMetaMask) {
+          let wallet = await ethereum.request({ method: "eth_requestAccounts" });
+          this.signature=await this.signMessage()
+          if(this.signature){
+          this.data.value=wallet[0];
+          
+          }             
+   }
+   
+ } catch (error) {
+   
+ }
 },
    async update() {
       if (!this.isFieldValid()|| this.data.value ==="") {
-       // this.data.value = "";
-      
         return this.notifyErr(Messages.EVENT_ACTIONS.INVALID_INPUT);
       } else {
           try {
-            let balance = await this.hasBalance()
-            
+            let balance = await this.hasBalance()            
             if(balance.result!==undefined){
               let wallet_balance= Number.parseInt(balance.result)
              if(wallet_balance > 0 ){
@@ -160,7 +165,9 @@ invokeMetamask(){
         const body ={
           "actionType": this.data.type,
           "data":  this.data.value,
-          "contractAddress": this.data.contractAddress
+          "contractAddress": this.data.contractAddress,
+          "signature":this.signature,
+          "message":this.message_sign,
         };
         let url = `${this.$config.studioServer.BASE_URL}api/v1/action/verify`;
       
