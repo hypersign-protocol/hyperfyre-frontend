1bbe9b9a5601e84ec562f1d4980f7ae252954bdf
rajpatil6939 <raj.v.patil108@gmail.com>
2022-02-10 10:23:13 +0530

    table added at prize section at participant side

--- src/components/participant/ActionInputs/PrizeCard.vue
+++ src/components/participant/ActionInputs/PrizeCard.vue
@@ -9,7 +9,7 @@
     >
       <b-row>
         <b-col cols="1" sm="1" md="1">
-          <i class="fas fa-gift" height="25px"></i>
+          <img src="../../../assets/gift.svg" height="25px" />
         </b-col>
         <b-col cols="9" sm="9" class="text-left" md="9">
           <div class="text">Prize Details</div>
@@ -18,24 +18,30 @@
     </b-card-header>
     <b-collapse id="profile" v-model="visible"  >
       <b-card-body class="user-details">
-        
-        <b-row v-for="row in prizeData" :key="row._d">
-          <b-col cols="12" sm="3" md="3">
-            <div class="title text-left mb-1">Prize Name</div>
-            <div class="text text-left">{{ row.title }}</div>
-          <hr>
-          </b-col>
-          <b-col cols="12" sm="4" md="4">
-            <div class="title text-left mb-1">Number of Winners</div>
-            <div class="text text-left">{{ JSON.parse(row.value).winners }}</div>
-             <hr>
-          </b-col>
-          <b-col cols="12" sm="5" md="5">
-            <div class="title text-left mb-1">Prize Per Winner</div>
-            <div class="text text-left">{{ JSON.parse(row.value).prizeValue }}</div>
-           <hr>
-          </b-col>
-        </b-row>
+        <table
+          v-if="prizeData.length"
+          class="table table-bordered"
+          style="background: #ffff"
+        >
+          <thead class="thead-light">
+            <tr>
+              <th>Prize Name</th>
+              <th>Number of winners</th>
+              <th>Prize Per Winner</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="row in prizeData" :key="row._id">
+              <th>
+                {{ row.title }}
+              </th>
+              <td>{{ JSON.parse(row.value).winners }}</td>
+              <td>
+                {{ JSON.parse(row.value).prizeValue }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </b-card-body>
     </b-collapse>
   </b-card>
@@ -52,23 +58,7 @@ export default {
   data() {
     return {
       visible: false,
-      value:{
-        winners:'',
-        prizeValue:''
-      }
     };  
-  },
-  mounted() {
-
-    // this.prizeData1 = this.prizeData;
-    // console.log(this.prizeData1);
-    //  if(prizeData){
-    //    Object.assign(this.value, {...JSON.parse(prizeData.value) })
-       
-    // }
-    // console.log(this.value);
-  // //   // this.detail.push(this.value)
-  // // console.log(this.detail);
   },
 };
 </script>
