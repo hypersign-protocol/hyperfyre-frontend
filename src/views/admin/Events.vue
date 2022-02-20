037cb8ebb3f031d1ab872954441ad55b614f7f37
Vishwas <vishwasanand.cse@gmail.com>
2022-02-20 08:52:58 +0530

    fixed the css of select drop down

--- src/views/admin/Events.vue
+++ src/views/admin/Events.vue
@@ -30,9 +30,6 @@
 .floatRight {
   float: right;
 }
-#floatRight {
-  float: right;
-}
 .card-header {
   background: aliceblue;
   padding: 0px;
@@ -172,8 +169,7 @@ i {
       </div>
 
       <div class="col-md-3">
-        <div id="floatRight">
-
+        <div>
           <multiselect
             v-model="selected"
             placeholder="Search events by tags"
