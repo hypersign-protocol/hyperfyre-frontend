206c11b65189b7c8c24cb4f6ae5d320738234382
varsha <varshakumari370@gmail.com>
2022-07-20 15:54:16 +0530

    replaced plus.svg to ff icon to change its color according to butText color

--- src/components/participant/ActionInputs/InputText.vue
+++ src/components/participant/ActionInputs/InputText.vue
@@ -15,7 +15,7 @@
         </b-col>
         <b-col cols="2" sm="2" md="2">
           <b-badge class="btn-score" :style="buttonThemeCss" @click="update()" v-if="!done">
-            <img src="../../../assets/plus.svg" />
+          <i class="fa fa-plus" aria-hidden="true"></i>
             {{ data.score }}
           </b-badge>
           <img
