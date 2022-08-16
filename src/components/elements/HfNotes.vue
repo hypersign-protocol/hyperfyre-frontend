3c01ee6d580e3e7e9b2cbd9a47bccad0518f2a32
rajpatil6939 <raj.v.patil108@gmail.com>
2022-08-16 11:49:12 +0530

    HfNotes Component optimization done

---
+++ src/components/elements/HfNotes.vue
@@ -0,0 +1,23 @@
+<template>
+  <p
+    class="my-4 border rounded-lg p-2 modal-text"
+    v-html="notes"
+  >
+    {{ notes }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    notes: String,
+  },
+};
+</script>
+
+<style>
+.modal-text {
+  font-size: 12px;
+  background: #f0f9ff;
+}
+</style>
---
+++ src/utils/messages/admin/Notes.js
@@ -0,0 +1,9 @@
+const LOTTERY_NOTES=`<b>Lottery</b> is the process of selecting winners of an event. Upon
+clicking on "<b>Execute button</b>", the lottery process begins, which
+may take sometime and screen may freeze. Once done, you will get
+selected records in excel sheet. The winner selection is primarly based
+on their "<b>score</b>" unless checked "<b>randomly</b>".`
+
+module.exports={
+    LOTTERY_NOTES
+}
