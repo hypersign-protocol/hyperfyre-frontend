0c87730628afb052bdae3eb53168bdd50369ddf8
rajpatil6939 <raj.v.patil108@gmail.com>
2022-09-20 13:44:59 +0530

    project api calling 2 times bug fixed in participants.vue page

--- src/views/admin/Participants.vue
+++ src/views/admin/Participants.vue
@@ -412,9 +412,11 @@ computed:{
     if (this.$route.query.projectId) {
       this.selectedProjectId = this.$route.query.projectId;
       this.selectedProject = this.$route.query.projectId;
+      if(!this.$route.query.projectId){
+        await this.fetchProjectData(0, this.perPage);
+      }
       eventBus.$emit("setForParticipant",this.selectedProject);
       this.investor.projectId = this.$route.query.projectId;
-      await this.fetchProjectData(0, this.perPage);
       this.holdInvestors = this.project.investors;
     }
 
