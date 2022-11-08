a4ea068f47929d3ebea3579e9cf06f43a7d87dbc
raj6939 <raj.v.patil108@gmail.com>
2022-11-08 19:47:31 +0530

    custom api configuratiion integrated at admin side (initial commit)

---
+++ src/components/admin/createProjectSlider/components/CustomApiEventActionConfig .vue
@@ -0,0 +1,452 @@
+<template>
+  <div>
+    <div
+      v-if="eventActionList.length"
+      style="overflow-y: auto"
+      class="selected-media-wrapper d-flex p-2 mb-4"
+    >
+      <div
+        @click="handleEventActionClick(idx)"
+        v-for="(eventAction, idx) in eventActionList"
+        v-bind:Key="idx"
+      >
+        <div
+          v-if="!eventActionList[idx].isDeleted"
+          :class="
+            flash == idx
+              ? 'flash card rounded m-1 p-1 d-flex flex-row align-items-center'
+              : 'card rounded m-1 p-1 d-flex flex-row align-items-center pointer'
+          "
+          style="min-width: 120px"
+        >
+          <span>
+            <img
+              style="padding-right: 5px"
+              src="/img/ethereum.2b470564.svg"          
+              height="22px"
+            />  
+          </span>
+          <span>{{ truncate1(eventAction.title, 6) }}</span>
+          <span style="color: gray; padding-left: 5px"
+            ><i style="" class="fas fa-minus-circle"></i
+          ></span>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <div class="row g-3 align-items-center w-100 mt-4">
+        <div class="text-left col-lg-3 col-md-3 text-left">
+          <label for="type" class="col-form-label"
+            >Title<span style="color: red">*</span>:
+          </label>
+        </div>
+        <div class="col-lg-9 col-md-9 px-0">
+          <input
+            v-model="selected.title"     
+            type="text"
+            id="title"
+            class="form-control w-100"
+            placeholder="Enter Action Title"
+          />
+        </div>
+      </div>
+
+      <div
+        class="row g-3 align-items-center w-100 mt-4"
+      >
+        <div class="text-left col-lg-3 col-md-3 text-left">
+          <label for="title" class="col-form-label"
+            >API Endpoint<span style="color: red">*</span>:
+          </label>
+        </div>
+        <div class="col-lg-9 col-md-9 px-0">
+          <input
+            v-model="selected.apiEndPoint"    
+            type="text"
+            id="title"
+            class="form-control w-100"
+            placeholder="Enter Api Endpoint"
+          />
+        </div>
+      </div>
+
+     <div
+        class="row g-3 align-items-center w-100 mt-4"
+      >
+        <div class="text-left col-lg-3 col-md-3 text-left">
+          <label for="title" class="col-form-label"
+            >Header (Optional):
+          </label>
+        </div>
+        <div class="col-lg-9 col-md-9 px-0">
+          <input
+            v-model="selected.header"
+            type="text"
+            id="title"
+            class="form-control w-100"
+            placeholder="Enter header"
+          />
+        </div>
+      </div>
+
+      <div
+        class="row g-3 align-items-center w-100 mt-4"
+      >
+        <div class="text-left col-lg-3 col-md-3 text-left">
+          <label for="type" class="col-form-label"
+            >API Method<span style="color: red">*</span>:
+          </label>
+        </div>
+        <div class="col-lg-9 col-md-9 px-0">
+          <hf-select-drop-down
+          :options="allCondition"
+          @selected=" e =>inputMethod(e)"          
+          ></hf-select-drop-down>
+        </div>
+      </div>
+
+      <div
+        class="row g-3 align-items-center w-100 mt-4"
+      >
+        <div class="text-left col-lg-3 col-md-3 text-left">
+          <label for="title" class="col-form-label"
+            >Condition<span style="color: red">*</span>:
+          </label>
+        </div>        
+          <div class="col-lg-2 col-md-2 px-0">
+          <hf-select-drop-down
+          :options="returnTypeOption"
+          @selected=" e =>inputReturnType(e)"  
+
+          ></hf-select-drop-down>          
+        </div>
+        <div class="col-lg-2 col-md-2 px-0">
+          <hf-select-drop-down
+          :options="condtionOption"
+          @selected=" e =>inputCondtion(e)"
+          ></hf-select-drop-down>    
+        </div>
+      </div>
+
+      <div class="row g-3 align-items-center w-100 mt-4">
+        <div class="text-left col-lg-3 col-md-3 text-left">
+          <label for="title" class="col-form-label"
+            >Score<span style="color: red">*</span>:
+          </label>
+        </div>
+        <div class="col-lg-9 col-md-9 px-0">
+          <input
+            v-model="selected.score"
+            type="number"
+            id="title"
+            class="form-control w-100"
+          />
+        </div>
+      </div>
+      <div
+        class="row g-3 justify-content-md-end w-100 mt-4"
+        v-if="isCreate == true"
+      >
+        <div class="col-lg-6 col-md-9 px-0">
+          <hf-buttons
+          name="Add"
+          @executeAction="handleEventActionAdd()"
+          ></hf-buttons>
+        </div>
+      </div>
+      <div class="row g-3 justify-content-md-end w-100 mt-4" v-else>
+        <div class="col-lg-6 col-md-9 px-0">
+          <hf-buttons
+          name="Update"
+          @executeAction="handleEventActionUpdate()"
+          customClass="btn button-theme slight-left-margin"
+          ></hf-buttons>
+          <hf-buttons
+          name="Delete"
+          @executeAction="handleEventActionDelete()"
+          customClass="btn btn-danger slight-left-margin"
+          ></hf-buttons>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.inputInfo {
+  color: #808080b5;
+  font-size: smaller;
+}
+
+.pointer {
+  cursor: pointer;
+}
+.selected-media-wrapper {
+  border: 1px dashed;
+  max-height: 100px;
+  background-color: #f5f5f5;
+  border-radius: 10px;
+}
+.card i {
+  cursor: pointer;
+}
+.card {
+  transition: all 0.5s;
+}
+.card i {
+  margin-right: 4px;
+}
+.flash {
+  cursor: pointer;
+  background-color: #1faa596b;
+  border: 0;
+  box-shadow: 2px 0 10px rgb(0 0 0 / 10%);
+  animation: flash 0.4s cubic-bezier(1, 0, 0, 1);
+}
+
+.fa-minus-circle {
+  font-size: 14px;
+}
+@keyframes flash {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.button-theme {
+  background-color: var(--button-bg-color);
+  border-collapse: var(--button-bg-color);
+  color:var(--button-text-color);;
+  border: 0;
+}
+.slight-left-margin {
+  margin-left: 2px;
+}
+</style>
+<script>
+import notificationMixins from "../../../../mixins/notificationMixins";
+import {
+  isEmpty,
+  isValidURL,
+  truncate,
+} from "../../../../mixins/fieldValidationMixin";
+import Messages from "../../../../utils/messages/admin/en";
+import config from "../../../../config"
+import HfButtons from "../../../elements/HfButtons.vue"
+import EventBus from '../../../../eventBus';
+import HfSelectDropDown from "../../../elements/HfSelectDropDown.vue"
+export default {
+  name: "CustomAPIEventActionConfig",
+  components: { HfButtons, HfSelectDropDown},
+  props: {
+    eventActionType: {
+      type: String,
+    },
+    eventActionList: {
+      type: Array,
+      required: true,
+    },
+    // options: {
+    //   type: Array,
+    // },
+  },
+  computed: {
+      buttonThemeCss() {
+      return {
+        '--button-bg-color': config.app.buttonBgColor,
+        '--button-text-color':config.app.buttonTextColor
+      }
+     },
+  },
+  data() {
+    return {
+      appName: config.appName,
+      allCondition: [
+        { text: "Select Method", value: null },
+        { text: "Get", value: "CUSTOM_API_GET" },
+        { text: "Post", value: "CUSTOM_API_POST" },
+      ],
+      returnTypeOption:[
+        { text: "None", value: null },
+        { text: "Boolean", value: "CUSTOM_API_RETURN_TYPE_BOOLEAN" },
+        { text: "String", value: "CUSTOM_API_RETURN_TYPE_STRING" },
+        { text: "Integer", value: "CUSTOM_API_RETURN_TYPE_INTEGER" },      
+      ],
+      condtionOption:[
+        { text: "None", value: null },
+        { text: "===", value: "===" },
+        { text: "<", value: "<" },
+        { text: ">", value: ">" },
+        { text: ">=", value: ">=" },
+        { text: "<=", value: "<=" },
+      ],      
+      flash: null,
+      isCreate: true,
+      currentSelectedId: 0,      
+      selected: {
+        type:"CUSTOM_API",  
+        title: "",
+        apiEndPoint: "",
+        isManadatory: true,
+        header:"",
+        apiMethod:null,
+        returnType:null,
+        condition:null,
+        score: 10,
+        id: "",
+        slug: "",
+      },
+      project:{},
+      hfTgBotId: this.$config.verifierBot.TELEGRAM,
+    };
+  },
+  async mounted() {
+    this.$root.$on("callClearFromProject", () => {
+      this.clearSelected();
+    });
+     EventBus.$on("sendProject", (project) => {
+      this.project = {...project}
+    });
+  },
+  methods: {
+    inputCondtion(e){
+      this.selected.condition = e
+    },
+    inputReturnType(e) {
+      this.selected.returnType = e
+    },
+    inputMethod(e){
+      this.selected.apiMethod = e
+    },
+    clearSelected() {
+      this.flash = null;
+      this.isCreate = true;
+      this.selected = {
+        type:"CUSTOM_API",  
+        title: "",
+        apiEndPoint: "",
+        isManadatory: true,
+        header:"",
+        apiMethod:null,
+        returnType:null,
+        condition:null,
+        score: 10,
+        id: "",
+        slug: "",
+      };
+    },
+    handleEventActionValidation() {
+      let isvalid = true;
+
+      //////
+      //// WARNINGS: This is worst way of handeling validation
+      //// You should return or break the moment first error occured
+      //// But here you are checking all validation every time - waste of time!
+      ////////////
+          if(this.project.projectStatus ==false){
+            isvalid = false;
+            return this.notifyErr(Messages.EVENTS.EVENT_CLOSED)
+          }
+          if (this.selected.type === null) {
+            isvalid = false;
+            this.notifyErr(
+              Messages.EVENTS.ACTIONS.SMARTCONTRACT.CHOOSE_CONTRACT_TYPE
+            );
+          }else if (this.contract.methods===null || isEmpty(this.contract.methods)) {
+            isvalid = false;
+            this.notifyErr(
+              Messages.EVENTS.ACTIONS.SMARTCONTRACT.METHODS_EMPTY
+            );
+          } else if (isEmpty(this.contract.contractAddress)) {
+            isvalid = false;
+            this.notifyErr(
+              Messages.EVENTS.ACTIONS.SMARTCONTRACT.ADDRESS_NOT_EMPTY
+            );
+          } else if (isEmpty(this.contract.contractABI)) {
+            isvalid = false;
+            this.notifyErr(
+              Messages.EVENTS.ACTIONS.SMARTCONTRACT.ABI_NOT_EMPTY
+            )
+          }else if (isValidURL(this.selected.title)) {
+            isvalid = false;
+            this.notifyErr(Messages.EVENTS.ACTIONS.TITLE_URL);
+          }else if (isEmpty(this.selected.title)) {
+            isvalid = false;
+            this.notifyErr(Messages.EVENTS.ACTIONS.EMPTY_TITLE);
+          }
+      return isvalid;
+    },
+
+    handleEventActionAdd() {
+      console.log(this.selected)
+      // Code to Add an Action
+      // let isvalid = this.handleEventActionValidation();
+      // if (isvalid) {        
+        this.selected["id"] = this.eventActionType + "_" + this.eventActionList.length;
+        this.eventActionType + "_" + this.eventActionList.length;
+        console.log(this.selected)
+        this.eventActionList.push(this.selected);
+        this.$emit("updateEventActions", {
+          type: "ADD",
+          data: this.selected,
+        });
+        EventBus.$emit("resetOption",this.selected.condition);
+        EventBus.$emit("resetOption",this.selected.returnType);
+        EventBus.$emit("resetOption",this.selected.apiMethod);
+        this.clearSelected();
+      // }
+    },
+    handleEventActionDelete() {
+      // Code to delete an Action
+      const actionToDelete = this.eventActionList[this.currentSelectedId];
+      this.eventActionList.splice(this.currentSelectedId, 1);
+      this.$emit("updateEventActions", {
+        type: "DELETE",
+        data: actionToDelete,
+      });
+        EventBus.$emit("resetOption",this.selected.condition);
+        EventBus.$emit("resetOption",this.selected.returnType);
+        EventBus.$emit("resetOption",this.selected.apiMethod);
+      this.clearSelected();
+      this.isCreate = true;
+    },
+    handleEventActionUpdate() {
+      // Code to update an Action
+      // let isvalid = this.handleEventActionValidation();
+      // if (isvalid) {
+        this.eventActionList[this.currentSelectedId] = this.selected;
+        this.$emit("updateEventActions", {
+          type: "UPDATE",
+          data: this.selected,
+        });
+        EventBus.$emit("resetOption",this.selected.condition);
+        EventBus.$emit("resetOption",this.selected.returnType);
+        EventBus.$emit("resetOption",this.selected.apiMethod);
+        this.clearSelected();
+        this.isCreate = true;
+      // }
+    },
+
+    handleEventActionClick(idx) {
+      this.isCreate = false;
+      // Code to update an Action
+      this.flash = idx;
+      let updateData = this.eventActionList[idx];
+      this.currentSelectedId = idx;
+      this.selected = updateData;
+      EventBus.$emit("setOption",this.selected.condition);
+      EventBus.$emit("setOption",this.selected.returnType);
+      EventBus.$emit("setOption",this.selected.apiMethod);
+    },
+
+    truncate1(str, number) {
+      return truncate(str, number);
+    },
+  },
+
+  mixins: [notificationMixins],
+};
+</script>
