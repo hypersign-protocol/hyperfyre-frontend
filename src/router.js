ad37603fa28b7d7963dcde101a3fc6b743c1ee38
Pratap2018 <pratapmridha@gmail.com>
2024-01-29 15:08:46 +0530

    update

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ import Auth from './components/login/Auth'
 Vue.use(Router);
 
 const router = new Router({
-    mode: "history",
+    mode: "hash",
     routes: [{
             path: "/form/:slug",
             name: "Event",
